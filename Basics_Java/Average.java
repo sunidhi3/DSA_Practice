f810c18651e189029b7ff23d9bb6156e91256211
sunidhi3 <chauhansunidhi223@gmail.com>
2025-11-01 15:28:39 +0530

    add new programs

---
+++ Basics_Java/Average.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        float principal=sc.nextFloat();
+        float rate=sc.nextFloat();
+        float time=sc.nextFloat();
+        float si=(principal*rate*time)/100;
+        System.out.println("simple interest : "+si);
+    }
+}
---
+++ Basics_Java/ProductOfTwo.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ProductOfTwo {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("product of two numbers"+":"+(a*b));
+    }
+}
